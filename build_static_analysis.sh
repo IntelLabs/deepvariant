efa554512a9fd95508a116ce3ac1f3072a40918f
Michael Beale <michael.beale@intel.com>
2023-06-13 10:29:09 -0700

    Update build_static_analysis.sh

--- build_static_analysis.sh
+++ build_static_analysis.sh
@@ -15,7 +15,14 @@ if ! bazel; then
   PATH="$HOME/bin:$PATH"
 fi
 
-
+# shellcheck disable=SC2086
+bazel build -c opt \
+  --output_filter=DONT_MATCH_ANYTHING \
+  --noshow_loading_progress \
+  --show_result=0 \
+  ${DV_COPT_FLAGS} \
+  --build_python_zip \
+  :binaries
 
 echo 'Expect a usage message:'
 (python3 bazel-out/k8-opt/bin/deepvariant/call_variants.zip --help || : ) | grep '/call_variants.py:'
